804af5f27941cb97ab1d9bd8d27cc9d5e0052027
Alex Todd <atodd@atodd.io>
2025-05-26 16:56:08 -0500

    Guestimate speed & distance from power

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { Decoder, Stream } from '@garmin/fitsdk'
+import { Decoder, Encoder, Profile, Stream } from '@garmin/fitsdk'
 import { useState } from 'react'
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null)
-  const [distance, setDistance] = useState<number>(0)
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -13,8 +12,19 @@ export default function Home() {
     }
   }
 
-  const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDistance(parseFloat(event.target.value))
+  const roundToThreeDecimalPlaces = (value: number): number => {
+    return Math.round(value * 1000) / 1000
+  }
+
+  const roundToTwoDecimalPlaces = (value: number): number => {
+    return Math.round(value * 100) / 100
+  }
+
+  const getMesgNumKey = (key: string): string => {
+    return key
+      .replace(/Mesgs$/, '')
+      .replace(/([a-z])([A-Z])/g, '$1_$2')
+      .toUpperCase()
   }
 
   const handleUpdate = async () => {
@@ -43,34 +53,121 @@ export default function Home() {
 
           const { messages } = decoder.read()
 
-          console.log('MESSAGES', messages)
+          let lastTimestamp: number | null = null
+          let totalDistance = 0
+          let maxSpeed = 0
 
-          const distanceInMeters = distance * 1609.34
-          messages.sessionMesgs[0].totalDistance = distanceInMeters
+          const recordMessages = messages.recordMesgs
+          recordMessages.forEach((message) => {
+            // Caclulate speed based on power.
+            const power: number = message.power
+            let speed: number
+            if (power <= 20) {
+              speed = power * 0.19
+            } else if (power <= 50) {
+              speed = power * 0.13
+            } else if (power <= 100) {
+              speed = power * 0.085
+            } else if (power <= 180) {
+              speed = power * 0.0614
+            } else if (power <= 250) {
+              speed = power * 0.0429
+            } else {
+              speed = power * 0.0359
+            }
 
-          // const encoder = new Encoder()
+            speed = roundToThreeDecimalPlaces(speed)
+            message.speed = speed
 
-          // // TODO: Write messages.
+            if (speed > maxSpeed) {
+              maxSpeed = speed
+            }
 
-          // const uint8Array = encoder.close()
-          // const blob = new Blob([uint8Array], {
-          //   type: 'application/octet-stream'
-          // })
+            // Calculate distance based on speed.
+            const timestamp = new Date(message.timestamp).getTime() / 1000
+            let deltaTime = 1
+            if (lastTimestamp !== null) {
+              deltaTime = timestamp - lastTimestamp
+            }
+            lastTimestamp = timestamp
 
-          // const originalName = file.name.replace(/\.fit$/i, '')
-          // const editedName = `${originalName}-edited.fit`
+            totalDistance += speed * deltaTime
+            message.distance = roundToTwoDecimalPlaces(totalDistance)
+          })
 
-          // const url = URL.createObjectURL(blob)
-          // const a = document.createElement('a')
-          // a.href = url
-          // a.download = editedName
-          // document.body.appendChild(a)
-          // a.click()
-          // document.body.removeChild(a)
-          // URL.revokeObjectURL(url)
+          const lapMessage = messages.lapMesgs?.[0]
+          if (lapMessage) {
+            lapMessage.totalDistance = roundToTwoDecimalPlaces(totalDistance)
+
+            const totalElapsedTime = lapMessage.totalElapsedTime
+            lapMessage.avgSpeed = roundToTwoDecimalPlaces(totalDistance / totalElapsedTime)
+            lapMessage.maxSpeed = roundToTwoDecimalPlaces(maxSpeed)
+          }
+
+          const sessionMessage = messages.sessionMesgs?.[0]
+          if (sessionMessage) {
+            sessionMessage.totalDistance = roundToTwoDecimalPlaces(totalDistance)
+
+            const totalElapsedTime = sessionMessage.totalElapsedTime
+            sessionMessage.avgSpeed = roundToTwoDecimalPlaces(totalDistance / totalElapsedTime)
+            lapMessage.maxSpeed = roundToTwoDecimalPlaces(maxSpeed)
+          }
+
+          const encoder = new Encoder()
+          Object.keys(messages).forEach((key) => {
+            const mesgNumKey = getMesgNumKey(key)
+            const mesgNum: number = Profile.MesgNum[mesgNumKey]
+
+            // Session
+            if (mesgNum == 18) {
+              encoder.onMesg(mesgNum, sessionMessage)
+              return
+            }
+
+            // Lap
+            if (mesgNum == 19) {
+              encoder.onMesg(mesgNum, lapMessage)
+              return
+            }
+
+            // Record
+            if (mesgNum == 20) {
+              recordMessages.forEach((message) => {
+                encoder.onMesg(mesgNum, message)
+              })
+              return
+            }
+
+            const keyMessages = messages[key] ?? []
+            keyMessages.forEach((message) => {
+              if (!message || Object.keys(message).length === 0) {
+                return
+              }
+
+              encoder.onMesg(mesgNum, message)
+            })
+            return
+          })
+
+          const uint8Array = encoder.close()
+          const blob = new Blob([uint8Array], {
+            type: 'application/octet-stream'
+          })
+
+          const originalName = file.name.replace(/\.fit$/i, '')
+          const editedName = `${originalName}-edited.fit`
+
+          const url = URL.createObjectURL(blob)
+          const a = document.createElement('a')
+          a.href = url
+          a.download = editedName
+          document.body.appendChild(a)
+          a.click()
+          document.body.removeChild(a)
+          URL.revokeObjectURL(url)
         }
       } catch (error) {
-        console.error('Error:', error)
+        console.error(error)
         alert('An unexpected error occurred. Please try again.')
         return
       }
@@ -82,7 +179,9 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-md rounded-lg p-6 max-w-sm min-w-[500px]">
         <h1 className="text-2xl font-bold">FIT Distance</h1>
-        <p className="text-gray-600 mb-4">Update the distance on a FIT file.</p>
+        <p className="text-gray-600 mb-4">
+          Calculate speed and distance ±5% using power information. Based on data from a Stages SC3 bike.
+        </p>
         <label htmlFor="file-upload" className="block text-sm font-medium text-gray-900">
           Upload FIT file
         </label>
@@ -93,22 +192,6 @@ export default function Home() {
               type="file"
               accept=".fit"
               onChange={handleFileChange}
-              className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm"
-            />
-          </div>
-        </div>
-        <label htmlFor="new-distance" className="block text-sm font-medium text-gray-900 mt-4">
-          New Distance (miles)
-        </label>
-        <div className="mt-2">
-          <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-            <input
-              id="new-distance"
-              type="number"
-              min="0"
-              step="0.01"
-              defaultValue="0"
-              onChange={handleDistanceChange}
               className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm"
             />
           </div>
